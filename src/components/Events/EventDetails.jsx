cbca91dfba61564812e0da01ca24556aa6841c86
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-11-05 02:44:33 +0200

    disable automatic fetching after invalidating

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -26,6 +26,7 @@ export default function EventDetails() {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['events'],
+        refetchType: 'none',
       });
       navigate('/events');
     },
@@ -36,16 +37,22 @@ export default function EventDetails() {
   }
 
   if (isPendingEvent) {
-    <LoadingIndicator />;
+    return (
+      <div id='event-details-content' className='center'>
+        <LoadingIndicator />
+      </div>
+    );
   }
 
   if (isErrorEvent) {
-    <div id='event-details-content' className='center'>
-      <ErrorBlock
-        title='An error occurred'
-        message={eventError.info?.message || 'Failed to fetch event'}
-      />
-    </div>;
+    return (
+      <div id='event-details-content' className='center'>
+        <ErrorBlock
+          title='An error occurred'
+          message={eventError.info?.message || 'Failed to fetch event'}
+        />
+      </div>
+    );
   }
 
   const formattedDate = new Date(event?.date).toLocaleDateString('en-US', {
@@ -62,32 +69,35 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {event && (
-        <article id='event-details'>
-          <header>
-            <h1>{event.title}</h1>
-            <nav>
-              <button onClick={handleDelete}>Delete</button>
-              <Link to='edit'>Edit</Link>
-            </nav>
-          </header>
-          <div id='event-details-content'>
-            <img
-              src={`http://localhost:3000/${event.image}`}
-              alt='event image'
-            />
-            <div id='event-details-info'>
-              <div>
-                <p id='event-details-location'> {event.location}</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>
-                  {formattedDate} @ {event.time}
-                </time>
+      <article id='event-details'>
+        {event && (
+          <>
+            <header>
+              <h1>{event.title}</h1>
+              <nav>
+                <button onClick={handleDelete}>Delete</button>
+                <Link to='edit'>Edit</Link>
+              </nav>
+            </header>
+            <div id='event-details-content'>
+              <img
+                src={`http://localhost:3000/${event.image}`}
+                alt='event image'
+              />
+              <div id='event-details-info'>
+                <div>
+                  <p id='event-details-location'> {event.location}</p>
+                  <time dateTime={`Todo-DateT$Todo-Time`}>
+                    {formattedDate} @ {event.time}
+                  </time>
+                </div>
+                <p id='event-details-description'>{event.description}</p>
               </div>
-              <p id='event-details-description'>{event.description}</p>
             </div>
-          </div>
-        </article>
-      )}
+          </>
+        )}
+      </article>
+      
     </>
   );
 }
