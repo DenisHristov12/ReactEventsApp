d5a8128cdf6982c9af9185985ad91f385fdd2ea0
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-11-05 02:10:14 +0200

    fetch event data

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,73 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../utils/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
+  const { id } = useParams();
+
+  console.log(id);
+
+  const {
+    data: event,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['event'],
+    queryFn: () => fetchEvent({ id }),
+  });
+
+  if (isPending) {
+    <LoadingIndicator />;
+  }
+
+  if (isError) {
+    <ErrorBlock
+      title='An error occurred'
+      message={error.info?.message || 'Failed to fetch event'}
+    />;
+  }
+
+  console.log(event);
+
   return (
     <>
       <Outlet />
       <Header>
-        <Link to="/events" className="nav-item">
+        <Link to='/events' className='nav-item'>
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {event && (
+        <article id='event-details'>
+          <header>
+            <h1>{event.title}</h1>
+            <nav>
+              <button>Delete</button>
+              <Link to='edit'>Edit</Link>
+            </nav>
+          </header>
+          <div id='event-details-content'>
+            <img
+              src={`http://localhost:3000/${event.image}`}
+              alt='event image'
+            />
+            <div id='event-details-info'>
+              <div>
+                <p id='event-details-location'> {event.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {event.date} at {event.time}
+                </time>
+              </div>
+              <p id='event-details-description'>{event.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -23,6 +23,38 @@ export async function fetchEvents({ signal, searchTerm }) {
   return events;
 }
 
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event data');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function deleteEvent({ id }) {
+  const response = fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
 export async function createNewEvent(eventData) {
   const response = await fetch(`http://localhost:3000/events`, {
     method: 'POST',
